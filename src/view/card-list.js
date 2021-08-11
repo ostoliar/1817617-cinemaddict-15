cc5b4633416c50de3b368ce6b0baff4108103d31
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-08-11 11:03:46 +0300

    update(*): module4 task1

--- src/view/card-list.js
+++ src/view/card-list.js
@@ -1,25 +1,8 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createTaskListTemplate = () => '<div class="films-list__container"></div>';
-
-export default class CardList {
-  constructor() {
-    this._element = null;
-  }
-
+export default class CardList extends AbstractView {
   getTemplate() {
     return createTaskListTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
